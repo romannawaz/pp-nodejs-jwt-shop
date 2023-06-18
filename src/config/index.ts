c256441c45c8dcc2ba1065c9033648c1c61cc746
romannawaz <romannawaz01@gmail.com>
2023-06-18 09:19:09 +0300

    feat: db connection

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,9 @@ dotenv.config();
 
 const SERVER_PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 
+const DB_URL = process.env.DATABASE_URL ?? '';
+
 export default {
   server: { port: SERVER_PORT },
+  db: { url: DB_URL },
 };
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,23 @@
 import express from 'express';
 import http from 'http';
 
+import mongoose from 'mongoose';
+
 import Logging from './libraries/logging';
 
 import config from './config';
 
 const app = express();
 
+mongoose.connect(config.db.url).then(() => {
+  Logging.info('Db is connected!');
+
+  StartServer();
+});
+
+/**
+ * Start when DB is connected
+ */
 const StartServer = () => {
   http
     .createServer(app)
@@ -14,5 +25,3 @@ const StartServer = () => {
       Logging.info(`Server is running on port ${config.server.port}.`),
     );
 };
-
-StartServer();
